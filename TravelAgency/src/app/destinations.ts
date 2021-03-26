771d8b98d5c58a474429750d490245b044c525b6
Natalia <natalia_bochenek@hotmail.com>
2021-03-26 11:22:42 +0100

    add images to file

--- TravelAgency/src/app/destinations.ts
+++ TravelAgency/src/app/destinations.ts
@@ -5,7 +5,8 @@ export const destinations = [
     destDescr: "2 weeks with nature, peace and the stunning Aurora borealis",
     startDate: new Date('12.04.2021'), // make it a Date() in travels.component.ts
     duration: 14,
-    price: 1.000
+    price: 1.000,
+    img: 'assets/img/destinations/iceland-joshua-earle-Hn8N4I4eHA0-unsplash.jpg'
   },
   {
     country: 'India',
@@ -13,7 +14,8 @@ export const destinations = [
     destDescr: "Indian spicies: Our beloved trip is available again",
     startDate: new Date('23.11.2025'), // make it a Date() in travels.component.ts
     duration: 21,
-    price: 3.300
+    price: 3.300,
+    img: 'assets/img/destinations/india-img.jpg'
   },
   {
     country: 'Ireland & Northern Ireland',
@@ -21,7 +23,8 @@ export const destinations = [
     destDescr: "Discover the birth-place of Titanic and dive into the secrets of Guinness",
     startDate: new Date('01.03.2023'), // make it a Date() in travels.component.ts
     duration: 10,
-    price: 1.100
+    price: 1.100,
+    img: 'assets/img/destinations/ireland-img-4.jpg'
   },
   {
     country: 'Japan',
@@ -29,7 +32,8 @@ export const destinations = [
     destDescr: "Explore the Hokkaido, Okinawa and Tokio in two weeks, incl. a cooking lesson.",
     startDate: new Date('23.11.2025'), // make it a Date() in travels.component.ts
     duration: 16,
-    price: 4.100
+    price: 4.100,
+    img: 'assets/img/destinations/japan-andre-benz-MsMISAIe8Qw-unsplash.jpg'
   },
   {
     country: 'Morocco',
@@ -37,7 +41,8 @@ export const destinations = [
     destDescr: "Follow the stories of the famous Scheherazade on this unique trip.",
     startDate: new Date('17.01.2022'), // make it a Date() in travels.component.ts
     duration: 15,
-    price: 1.800
+    price: 1.800,
+    img: 'assets/img/destinations/marocco-img-8.jpg'
   },
   {
     country: 'Mongolia',
@@ -45,7 +50,8 @@ export const destinations = [
     destDescr: "Sleeping in a Yurt, riding a horse, exploring the wide steps of Mongolia.",
     startDate: new Date('20.11.2025'), // make it a Date() in travels.component.ts
     duration: 21,
-    price: 2.600
+    price: 2.600,
+    img: 'assets/img/destinations/mongolia-redcharlie-O7zkyNkQ1lM-unsplash.jpg'
   },
   {
     country: 'USA',
@@ -53,6 +59,7 @@ export const destinations = [
     destDescr: "A round trip in the West of USA, from San Fransisco trough, Death Valley, Las Vegas up to Grand Canyon and more.",
     startDate: new Date('06.06.2024'), // make it a Date() in travels.component.ts
     duration: 18,
-    price: 3.100
+    price: 3.100,
+    img: 'assets/img/destinations/usa-omer-nezih-gerek-ZZnH4GOzDgc-unsplash.jpg'
   }
 ];
