33654e713545e7b024a39591d3f20ea9382d6036
Natalia <natalia_bochenek@hotmail.com>
2021-03-26 08:54:17 +0100

    add routing to home and blog pages

--- TravelAgency/src/app/app-routing.module.ts
+++ TravelAgency/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'blog', component: BlogComponent
+  } 
+  // add travels incl. travelId later 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
