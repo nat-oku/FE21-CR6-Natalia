77cc635bba1f5f9c697bb118c275c216dfa3176a
Natalia <natalia_bochenek@hotmail.com>
2021-03-26 11:15:24 +0100

    add data to destinations

---
+++ TravelAgency/src/destinations.ts
@@ -0,0 +1,58 @@
+export const destinations = [
+  {
+    country: 'Iceland',
+    destName: "Iceland nature & more",
+    destDescr: "2 weeks with nature, peace and the stunning Aurora borealis",
+    startDate: new Date('12.04.2021'), // make it a Date() in travels.component.ts
+    duration: 14,
+    price: 1.000
+  },
+  {
+    country: 'India',
+    destName: "India & Taj Mahal",
+    destDescr: "Indian spicies: Our beloved trip is available again",
+    startDate: new Date('23.11.2025'), // make it a Date() in travels.component.ts
+    duration: 21,
+    price: 3.300
+  },
+  {
+    country: 'Ireland & Northern Ireland',
+    destName: "A round trip you won't forget",
+    destDescr: "Discover the birth-place of Titanic and dive into the secrets of Guinness",
+    startDate: new Date('01.03.2023'), // make it a Date() in travels.component.ts
+    duration: 10,
+    price: 1.100
+  },
+  {
+    country: 'Japan',
+    destName: "Japanese cherry dream",
+    destDescr: "Explore the Hokkaido, Okinawa and Tokio in two weeks, incl. a cooking lesson.",
+    startDate: new Date('23.11.2025'), // make it a Date() in travels.component.ts
+    duration: 16,
+    price: 4.100
+  },
+  {
+    country: 'Morocco',
+    destName: "Arabian dreams vol. 4",
+    destDescr: "Follow the stories of the famous Scheherazade on this unique trip.",
+    startDate: new Date('17.01.2022'), // make it a Date() in travels.component.ts
+    duration: 15,
+    price: 1.800
+  },
+  {
+    country: 'Mongolia',
+    destName: "Beautiful Mongolia",
+    destDescr: "Sleeping in a Yurt, riding a horse, exploring the wide steps of Mongolia.",
+    startDate: new Date('20.11.2025'), // make it a Date() in travels.component.ts
+    duration: 21,
+    price: 2.600
+  },
+  {
+    country: 'USA',
+    destName: "GTA - Grand Tourist Attraction",
+    destDescr: "A round trip in the West of USA, from San Fransisco trough, Death Valley, Las Vegas up to Grand Canyon and more.",
+    startDate: new Date('06.06.2024'), // make it a Date() in travels.component.ts
+    duration: 18,
+    price: 3.100
+  }
+];
